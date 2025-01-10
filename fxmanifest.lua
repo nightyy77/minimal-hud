f4e39ba836a138dfa7700c0feb7e6614349a3f62
vipexv <101529155+vipexv@users.noreply.github.com>
2025-01-10 20:15:51 +0100

    refactor(fxmanifest): use prettierd

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,35 +8,35 @@ version("1.1.0")
 repository("https://github.com/vipexv/minimal-hud")
 
 shared_scripts({
-	"require.lua",
+    "require.lua",
 })
 
 client_scripts({
-	"client/main.lua",
-	"client/nuicb.lua",
-	"client/commands.lua",
+    "client/main.lua",
+    "client/nuicb.lua",
+    "client/commands.lua",
 })
 
 server_scripts({
-	"server/main.lua",
+    "server/main.lua",
 })
 
 ui_page("dist/index.html")
 -- ui_page("http://localhost:5173/")
 
 files({
-	"dist/index.html",
-	"dist/assets/*.js",
-	"dist/assets/*.css",
-	"dist/**/*.woff2",
-	"config/shared.lua",
-	"config/functions.lua",
-	"modules/interface/client.lua",
-	"modules/utils/shared.lua",
-	"modules/seatbelt/client.lua",
-	"modules/frameworks/**/*.lua",
-	"modules/threads/client/**/*.lua",
-	"data/mapData.lua",
+    "dist/index.html",
+    "dist/assets/*.js",
+    "dist/assets/*.css",
+    "dist/**/*.woff2",
+    "config/shared.lua",
+    "config/functions.lua",
+    "modules/interface/client.lua",
+    "modules/utils/shared.lua",
+    "modules/seatbelt/client.lua",
+    "modules/frameworks/**/*.lua",
+    "modules/threads/client/**/*.lua",
+    "data/mapData.lua",
 })
 
 lua54("yes")
